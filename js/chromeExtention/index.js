591d4f4dc9a453b550dcb466fbfb47655cf58cc1
AdityaKumar8761 <S24CSEU1905@bennett.edu.in>
2026-08-19 17:11:13 +0530

    chrome exetention version1.1

--- js/chromeExtention/index.js
+++ js/chromeExtention/index.js
@@ -1,34 +1,74 @@
-let j = 0;
 
-let myLeads = []
+let myLeads = JSON.parse(localStorage.getItem("myLead")) || [];
 
-const ulEl = document.querySelector("#ul-el")
+const ulEl = document.querySelector("#ul-el");
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn");
+const deleteBtn = document.querySelector("#delete-btn");
+const tabBtn = document.querySelector("#saveTab-btn")
 
-const inputEl = document.querySelector("#input-el")
-const inputBtn = document.querySelector("#input-btn")
+renderLeads(myLeads)
 
-inputBtn.addEventListener("click", ()=>{
-    let value = inputEl.value
-    myLeads.push(value)
-    renderLeads()
-    inputEl.value = ""
+
+tabBtn.addEventListener("click" , function(){
+
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLead",JSON.stringify(myLeads))
+    renderLeads(myLeads)
+});
+
 })
 
-inputEl.addEventListener("keydown", (event)=>{
-if(event.key === "Enter"){
-    let value = inputEl.value
-    myLeads.push(value)
-    renderLeads()
-    inputEl.value = ""
+
+
+
+
+inputBtn.addEventListener("click", () => {
+    let value = inputEl.value;
+
+    if (value === "") return;
+
+    myLeads.push(value);
+    renderLeads(myLeads);
+
+    inputEl.value = "";
+    localStorage.setItem("myLead", JSON.stringify(myLeads));
+});
+
+inputEl.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        let value = inputEl.value;
+
+        if (value === "") return;
+
+        myLeads.push(value);
+        renderLeads(myLeads);
+
+        inputEl.value = "";
+        localStorage.setItem("myLead", JSON.stringify(myLeads));
+    }
+});
+
+function renderLeads(myLeads) {
+        let listItems = "";
+    
+        for (let i = 0; i < myLeads.length; i++) {
+            listItems += `
+                <li>
+                    <a target="_blank" href="${myLeads[i]}">
+                        ${myLeads[i]}
+                    </a>
+                </li>
+            `;
+        }
+    
+        ulEl.innerHTML = listItems;
+
 }
+
+deleteBtn.addEventListener("click" , ()=>{
+    localStorage.clear()
+    myLeads=[]
+    renderLeads(myLeads)
 })
-
-function renderLeads(){
-        ulEl.innerHTML += 
-        `<li>
-        <a target = '_blank' href= '${myLeads[j]}'> ${myLeads}
-        </li> `
-
-        j += 1;
-}
-
